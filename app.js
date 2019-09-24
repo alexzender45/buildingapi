aad22208becd11877d20d6f7e3ef3aa1f52cd515
alexzender45 <alexzendersamuel33@gmail.com>
2019-09-24 14:52:49 -0700

    added new features

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const  Recipe= require('./models/recipe');
 
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb+srv://data:data@cluster0-lmahg.mongodb.net/test?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://linus:nA7HxOKVadZBC9ST@cluster0-bi7s7.mongodb.net/test?retryWrites=true&w=majority')
 .then(() => {
   console.log('Successfully connected to Database!');
 })
